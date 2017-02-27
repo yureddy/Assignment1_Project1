291875e2de4da55aaa4560e2d001fbdfa5d64deb
Yugandhar <ygndr.reddy@gmail.com>
2017-02-26 23:15:00 -0500

    added the Sphinx auto documentation tool

--- source/Scrub.py
+++ source/Scrub.py
@@ -10,7 +10,11 @@ f = open('data.csv')
 csv_f = csv.reader(f)
 
 #  we are trying to loop over every row
+# every row is a list of three elemets, if you see closely
+
 for row in csv_f:
-    print row
+    print row[0]
+
+
 
 f.close()
